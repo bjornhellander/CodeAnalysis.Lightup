c94bed34fab2c8d8eef29269803a95d7d3548095
Björn Hellander <bjorn.hellander@gmail.com>
2024-03-02 20:00:23 +0100

    Added test project for Roslyn 4.8.0 (c# 12)

---
+++ Roslyn.CodeAnalysis.Lightup.Test/CSharp/LightupStatusTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Roslyn.CodeAnalysis.Lightup.CSharp;
+using System;
+using System.Reflection;
+
+namespace Roslyn.CodeAnalysis.Lightup.Test.CSharp
+{
+    [TestClass]
+    public class LightupStatusTests
+    {
+        [TestMethod]
+        public void TestLanguageVersion9()
+        {
+            var shouldBeSupported = GetExpectedSupportedLanguage() >= LanguageVersionEx.CSharp9;
+            Assert.AreEqual(shouldBeSupported, LightupStatus.SupportsCSharp9);
+        }
+
+        [TestMethod]
+        public void TestLanguageVersion10()
+        {
+            var shouldBeSupported = GetExpectedSupportedLanguage() >= LanguageVersionEx.CSharp10;
+            Assert.AreEqual(shouldBeSupported, LightupStatus.SupportsCSharp10);
+        }
+
+        [TestMethod]
+        public void TestLanguageVersion11()
+        {
+            var shouldBeSupported = GetExpectedSupportedLanguage() >= LanguageVersionEx.CSharp11;
+            Assert.AreEqual(shouldBeSupported, LightupStatus.SupportsCSharp11);
+        }
+
+        [TestMethod]
+        public void TestLanguageVersion12()
+        {
+            var shouldBeSupported = GetExpectedSupportedLanguage() >= LanguageVersionEx.CSharp12;
+            Assert.AreEqual(shouldBeSupported, LightupStatus.SupportsCSharp12);
+        }
+
+        private LanguageVersion GetExpectedSupportedLanguage()
+        {
+            var assemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+            var assemblyNameSuffix = assemblyName.Substring(32);
+            return assemblyNameSuffix switch
+            {
+                "" => LanguageVersion.CSharp8,
+                ".4_8_0" => LanguageVersionEx.CSharp12,
+                _ => throw new ArgumentException(),
+            };
+        }
+    }
+}
