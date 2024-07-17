1fa58f1f46cfe15498eddc50eba538d3ab1c227a
Björn Hellander <bjorn.hellander@gmail.com>
2024-07-17 17:16:55 +0200

    Added property CodeAnalysisVersion in LightupStatus

--- Roslyn.CodeAnalysis.Lightup.CSharp/LightupStatus.cs
+++ Roslyn.CodeAnalysis.Lightup.CSharp/LightupStatus.cs
@@ -11,11 +11,14 @@ namespace Roslyn.CodeAnalysis.Lightup.CSharp
 
         static LightupStatus()
         {
+            CodeAnalysisVersion = GetCodeAnalysisVersion();
             SupportsCSharp9 = IsLanguageVersionSupported(LanguageVersionEx.CSharp9);
             SupportsCSharp10 = IsLanguageVersionSupported(LanguageVersionEx.CSharp10);
             SupportsCSharp11 = IsLanguageVersionSupported(LanguageVersionEx.CSharp11);
             SupportsCSharp12 = IsLanguageVersionSupported(LanguageVersionEx.CSharp12);
         }
+
+        public static Version CodeAnalysisVersion { get; }
 
         public static bool SupportsCSharp9 { get; }
 
@@ -24,6 +27,13 @@ namespace Roslyn.CodeAnalysis.Lightup.CSharp
         public static bool SupportsCSharp11 { get; }
 
         public static bool SupportsCSharp12 { get; }
+
+        private static Version GetCodeAnalysisVersion()
+        {
+            var assembly = typeof(SyntaxKind).Assembly;
+            var version = assembly.GetName().Version;
+            return version;
+        }
 
         private static bool IsLanguageVersionSupported(LanguageVersion languageVersion)
         {
