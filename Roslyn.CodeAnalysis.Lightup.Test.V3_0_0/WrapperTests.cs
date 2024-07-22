238a151e6aea5948a0b1059519b0546e80218e37
Björn Hellander <bjorn.hellander@gmail.com>
2024-07-22 18:13:41 +0200

    Added test that makes sure that all generated static constructors are executed and that no exceptions are thrown

---
+++ Roslyn.CodeAnalysis.Lightup.Test.V3_0_0/WrapperTests.cs
@@ -0,0 +1,55 @@
+﻿namespace Roslyn.CodeAnalysis.Lightup.Test.V3_0_0;
+
+using System;
+using System.Linq;
+using Microsoft.CodeAnalysis.CSharp.Lightup;
+using Microsoft.CodeAnalysis.Lightup;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class WrapperTests
+{
+    // TODO: Add type from Workspaces.Common
+    // TODO: Add type from CSharp.Workspaces
+    [TestMethod]
+    [DataRow(typeof(OperationKindEx))] // From Common
+    [DataRow(typeof(SyntaxKindEx))] // From CSharp
+    public void TestStaticConstructor(Type exampleType)
+    {
+        var dummyObj = new object();
+
+        var assembly = exampleType.Assembly;
+        var types = assembly.GetTypes();
+        foreach (var type in types.Where(IsRelevantType))
+        {
+            var isMethod = type.GetMethod("Is");
+            if (isMethod != null)
+            {
+                isMethod.Invoke(null, [new object()]);
+            }
+        }
+    }
+
+    private static bool IsRelevantType(Type type)
+    {
+        if (type.IsEnum)
+        {
+            return false;
+        }
+
+        if (typeof(Attribute).IsAssignableFrom(type))
+        {
+            return false;
+        }
+
+        switch (type.Name)
+        {
+            case "LightupHelper":
+            case "LightupStatus":
+                return false;
+
+            default:
+                return true;
+        }
+    }
+}
---
+++ Roslyn.CodeAnalysis.Lightup.Test.V3_8_0/WrapperTests.cs
@@ -0,0 +1,8 @@
+﻿namespace Roslyn.CodeAnalysis.Lightup.Test.V3_8_0;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class WrapperTests : V3_0_0.WrapperTests
+{
+}
