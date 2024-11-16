afabad735ae43e9f126248a94b812b2ef9cb23e4
Björn Hellander <bjorn.hellander@gmail.com>
2024-11-16 12:09:04 +0100

    Added "tests" for Task, to document its behavior

---
+++ Roslyn.CodeAnalysis.Lightup.Test.Internal/TaskTests.cs
@@ -0,0 +1,40 @@
+﻿// Copyright © Björn Hellander 2024
+// Licensed under the MIT License. See LICENSE.txt in the repository root for license information.
+
+namespace Roslyn.CodeAnalysis.Lightup.Test.Internal;
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// NOTE: These tests are just here to document the behavior of Task
+[TestClass]
+public class TaskTests
+{
+    [TestMethod]
+    public void TestAntecedentThrowsExceptionBeforeFirstAwait()
+    {
+        Assert.ThrowsExceptionAsync<InvalidOperationException>(async () => await WillThrowInvalidOperationExeptionBeforeFirstAwait());
+        Assert.ThrowsExceptionAsync<InvalidOperationException>(async () => await WillThrowInvalidOperationExeptionBeforeFirstAwait().ContinueWith(x => x.Result));
+    }
+
+    [TestMethod]
+    public void TestAntecedentThrowsExceptionAfterFirstAwait()
+    {
+        Assert.ThrowsExceptionAsync<InvalidOperationException>(async () => await WillThrowInvalidOperationExeptionAfterFirstAwait());
+        Assert.ThrowsExceptionAsync<InvalidOperationException>(async () => await WillThrowInvalidOperationExeptionAfterFirstAwait().ContinueWith(x => x.Result));
+    }
+
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+    private static async Task<int> WillThrowInvalidOperationExeptionBeforeFirstAwait()
+    {
+        throw new InvalidOperationException();
+    }
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+
+    private static async Task<int> WillThrowInvalidOperationExeptionAfterFirstAwait()
+    {
+        await Task.Delay(0);
+        throw new InvalidOperationException();
+    }
+}
