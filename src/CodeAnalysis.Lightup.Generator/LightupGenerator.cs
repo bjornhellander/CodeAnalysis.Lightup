5b9c062034ffcf8319a4c0e1b895cd9e058d35ae
Björn Hellander <bjorn.hellander@gmail.com>
2025-01-01 11:30:23 +0100

    Updated the package version of the generator

--- src/CodeAnalysis.Lightup.Generator/LightupGenerator.cs
+++ src/CodeAnalysis.Lightup.Generator/LightupGenerator.cs
@@ -104,6 +104,7 @@ public class LightupGenerator : IIncrementalGenerator
             }
             else if (HasNewMembers(structTypeDef))
             {
+                // TODO: Use just "Ex" here as well? Not all members will be extension methods.
                 return ($"{typeDef.Name}Extensions", true);
             }
         }
