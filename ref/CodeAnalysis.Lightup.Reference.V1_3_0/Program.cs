844833deb89a51a033004289897771ba422eafc4
Björn Hellander <bjorn.hellander@gmail.com>
2025-01-05 18:39:00 +0100

    Replaced reference project for 1.3.2 with 1.3.0

--- ref/CodeAnalysis.Lightup.Reference.V1_3_0/Program.cs
+++ ref/CodeAnalysis.Lightup.Reference.V1_3_0/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright © Björn Hellander 2024
 // Licensed under the MIT License. See LICENSE.txt in the repository root for license information.
 
-namespace CodeAnalysis.Lightup.Reference.V1_3_2;
+namespace CodeAnalysis.Lightup.Reference.V1_3_0;
 
 internal class Program
 {
