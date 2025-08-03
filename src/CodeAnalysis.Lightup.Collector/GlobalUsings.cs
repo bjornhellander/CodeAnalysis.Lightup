ecd396ab4e76b3bc27f29939a80a236a2f784e90
Björn Hellander <bjorn.hellander@gmail.com>
2025-08-03 10:21:53 +0200

    Replaced nuget package StyleCop.Analyzers with NewStyleCop.Analyzers

--- src/CodeAnalysis.Lightup.Collector/GlobalUsings.cs
+++ src/CodeAnalysis.Lightup.Collector/GlobalUsings.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright © Björn Hellander 2024
 // Licensed under the MIT License. See LICENSE.txt in the repository root for license information.
 
-#pragma warning disable SA1200 // Using directives should be placed correctly
 global using System;
 global using System.Collections.Generic;
 global using System.Diagnostics.CodeAnalysis;
--- test/CodeAnalysis.Lightup.Test.V4_8_0/GlobalUsings.cs
+++ test/CodeAnalysis.Lightup.Test.V4_8_0/GlobalUsings.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright © Björn Hellander 2024
 // Licensed under the MIT License. See LICENSE.txt in the repository root for license information.
 
-#pragma warning disable SA1200 // Using directives should be placed correctly
 global using Microsoft.CodeAnalysis.CodeActions;
 global using Microsoft.CodeAnalysis.CodeActions.Lightup;
 global using Microsoft.CodeAnalysis.CSharp;
